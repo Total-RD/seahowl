ff299851a3b24cda5e788163b5543932450b61d0
Tristan de Lataillade <delataillade.tristan@gmail.com>
2023-02-22 11:37:53 +0100

    Fix eta interpolation issue for applying aero loads

--- src/elasto/elasto.cpp
+++ src/elasto/elasto.cpp
@@ -109,7 +109,7 @@ void ComponentElastoFEA::accumulate_element_load(chrono::ChVector<double> load,
     node0->SetForce(node0->GetForce() + load0);
     node0->SetTorque(node0->GetTorque() + (position + offset - node0->GetPos()) % load0);
     // load on second node
-    double weight1 = 0.5 * abs(eta - 1);
+    double weight1 = 0.5 * abs(eta + 1);
     auto load1 = load * weight1;
     auto node1 = std::dynamic_pointer_cast<chrono::fea::ChNodeFEAxyzrot>(element->GetNodeN(1));
     node1->SetForce(node1->GetForce() + load1);
