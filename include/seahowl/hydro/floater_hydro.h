e323e0fee841e0e2435457cac0503666f42ac393
Tristan de Lataillade <delataillade.tristan@gmail.com>
2023-04-11 15:45:22 +0200

    Attach HydroChrono variables to FloaterHydro

--- include/seahowl/hydro/floater_hydro.h
+++ include/seahowl/hydro/floater_hydro.h
@@ -3,14 +3,45 @@
 #include <string>
 
 #include <hydroc/hydro_forces.h>
+#include <seahowl/elasto/entities_elasto.h>
 
-class FloaterHydro {
+class FloaterHydroChrono {
   public:
-    std::string filepath_h5_potential;
-    std::string name;
-    TestHydro hydro;
+    /** @brief Hydro inputs (from HydroChrono). */
+    HydroInputs hydro_inputs;
+    /** @brief Path to potential flow frequency data file (HDF5 format). */
+    std::string filepath_h5_potential = "";
 
-    void set(std::vector<std::shared_ptr<chrono::ChBody>> bodies, HydroInputs& hydro_inputs) {
-        // hydro = TestHydro(bodies, filepath_h5_potential, hydro_inputs);
-    }
+    /**
+     * @brief Sets file path for potential flow frequency data in HDF5 format.
+     *
+     * @param[in] filepath Filepath to HDF5 file.
+     */
+    void set_h5filepath(const std::string& filepath);
+
+    /**
+     * @brief Adds body to floater.
+     *
+     * @param[in] body The body to pass (will be owned by floater).
+     * @param[in] body The name of the body in the hydro file.
+     */
+    void add_body(std::unique_ptr<seahowl::elasto::BodyElasto>&& body, std::string& name);
+
+    /**
+     * @brief Gets body.
+     *
+     * @param[in] body The name of the body to return.
+     */
+    seahowl::elasto::BodyElasto& get_body(std::string& name);
+
+    /**
+     * @brief Initializes floater.
+     */
+    void initialize();
+
+  private:
+    /** @brief List of bodies and their names. */
+    std::map<std::string, std::unique_ptr<seahowl::elasto::BodyElasto>> bodies_map;
+    /** @brief HydroChrono logic class. */
+    TestHydro hydrochrono_setter;
 };
--- src/hydro/floater_hydro.cpp
+++ src/hydro/floater_hydro.cpp
@@ -1,1 +1,36 @@
 #include "seahowl/hydro/floater_hydro.h"
+
+#include "seahowl/elasto/chrono_adapters.h"
+
+#include <chrono/physics/ChBody.h>
+
+void FloaterHydroChrono::set_h5filepath(const std::string& filepath) {
+    filepath_h5_potential = filepath;
+};
+
+void FloaterHydroChrono::add_body(std::unique_ptr<seahowl::elasto::BodyElasto>&& body, std::string& name) {
+    bodies_map[name] = std::move(body);
+}
+
+seahowl::elasto::BodyElasto& FloaterHydroChrono::get_body(std::string& name) {
+    return *bodies_map[name];
+}
+
+void FloaterHydroChrono::initialize() {
+    if (bodies_map.size() < 1) {
+        throw std::runtime_error("List of bodies for floater was not initialized.");
+    } else if (filepath_h5_potential == "") {
+        throw std::runtime_error("Path of h5 file for floater was not defined.");
+    }
+    std::vector<std::shared_ptr<chrono::ChBody>> chbodies = {};
+    for (auto& body : bodies_map) {
+        try {
+            auto chbody = dynamic_cast<seahowl::elasto::BodyElastoChrono&>(*body.second);
+            chbody.chobj->SetName(body.first.c_str());
+            chbodies.push_back(chbody.chobj);
+        } catch (const std::bad_cast& e) {
+            throw std::runtime_error("Body passed to floater is not a Chrono Body");
+        }
+    }
+    hydrochrono_setter = TestHydro(chbodies, filepath_h5_potential, hydro_inputs);
+}
