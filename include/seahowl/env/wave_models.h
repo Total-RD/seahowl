9aae81652d0ac2379e11bc8d4bc0f6711b9e4b59
Tristan de Lataillade <delataillade.tristan@gmail.com>
2023-07-04 11:12:39 +0200

    Add WaveModel and StillWater classes in env

---
+++ include/seahowl/env/wave_models.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "seahowl/commons/numerics.h"
+#include "seahowl/env/fluid_models.h"
+
+namespace seahowl {
+namespace env {
+
+/**
+ * @brief Base class for wave models
+ */
+class WaveModel : public FluidModel {
+  public:
+    /** @brief Water density. */
+    double density = 1025;
+};
+
+class StillWater : public WaveModel {
+  public:
+    /** @brief Mean water level. */
+    double mean_water_level = 0.0;
+
+    /**
+     * @brief Constructor.
+     */
+    StillWater();
+
+    virtual Vector3d get_fluid_velocity(const Vector3d& position, double time) const override;
+
+    virtual double get_fluid_density(const Vector3d& position, double time) const override;
+};
+
+}  // namespace env
+}  // namespace seahowl
---
+++ src/env/wave_models.cpp
@@ -0,0 +1,20 @@
+#include "seahowl/env/wave_models.h"
+
+using namespace seahowl::env;
+using seahowl::Vector3d;
+
+Vector3d StillWater::get_fluid_velocity(const Vector3d& position, double time) const {
+    if (position[2] <= mean_water_level) {
+        return Vector3d(0.0, 0.0, 0.0);
+    } else {
+        throw std::runtime_error("Cannot retrieve water velocity above mean water level.");
+    }
+}
+
+double StillWater::get_fluid_density(const Vector3d& position, double time) const {
+    if (position[2] <= mean_water_level) {
+        return density;
+    } else {
+        throw std::runtime_error("Cannot retrieve water density above mean water level.");
+    }
+}
