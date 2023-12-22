c9508e5dd1d89c842142866c0370e7357203ab41
Tristan de Lataillade <delataillade.tristan@gmail.com>
2023-12-22 09:29:11 +0100

    Add test for FPM blades

--- tests/unit_tests/test_components.cpp
+++ tests/unit_tests/test_components.cpp
@@ -369,6 +369,65 @@ TEST(test_turbine, rpm_initial_pitch) {
     ASSERT_NEAR(turbine.rna.elasto.get_rpm(), 2.746750, 1e-4);
 }
 
+TEST(test_turbine, rpm_initial_pitch_fpm) {
+    // general options
+    bool visualization_on = true;
+    bool statics_prestep = true;
+    // solver
+    auto verbose = false;
+    // timestepping
+    double dt = 0.1;
+    // wind
+    auto wind_model = seahowl::env::ConstantWind();
+    wind_model.set_wind_velocity(Vector3d(8.0, 0.0, 0.0));
+    // turbine
+    double initial_pitch = seahowl::PI / 8.0;
+
+    // system
+    auto system_elasto = SystemElastoChrono();
+    system_elasto.set_gravitational_acceleration(Vector3d(0.0, 0.0, -9.81));
+    auto system_chrono = system_elasto.chobj;
+
+    // turbine
+    auto turbine_elasto = seahowl::elasto::TurbineElasto();
+    auto turbine_aero = seahowl::aero::TurbineAero();
+    auto turbine = seahowl::core::Turbine(turbine_elasto, turbine_aero);
+    populate_turbine_from_json((DATADIR / "turbine_nocontrol_fpm.json").generic_string(), turbine);
+
+    // remove controller
+    turbine.controller = std::make_shared<seahowl::servo::Controller>();
+    turbine.build();
+    turbine.elasto.assemble(system_elasto);
+    turbine.tower.elasto.nodes.front()->set_fixed(true);
+
+    // statics
+    if (statics_prestep) {
+        turbine.rna.elasto.link_shaft_hub->set_constraints(true, true, true, true, true, true);
+        system_elasto.do_statics(true, 10);
+        turbine.rna.elasto.link_shaft_hub->set_constraints(true, true, true, false, true, true);
+    }
+
+    double time = 0.0;
+    turbine.rna.elasto.rotor->apply_collective_pitch_increment(initial_pitch);
+    turbine.initialize(time, dt);
+    // while (application.GetDevice()->run()) {
+    while (time < 50) {
+        // prestep
+        // compute forces
+        turbine.aero.compute_aero_loads(wind_model, time);
+        // prestep (accumulates loads from aero to elasto)
+        turbine.prestep(time, dt);
+
+        system_elasto.step(dt);
+        time += dt;
+
+        // poststep
+        turbine.poststep(time, dt);
+    }
+
+    ASSERT_NEAR(turbine.rna.elasto.get_rpm(), 2.750031, 1e-4);
+}
+
 TEST(test_turbine, rpm_initial_pitch_rigid_rotor) {
     // general options
     bool visualization_on = true;
