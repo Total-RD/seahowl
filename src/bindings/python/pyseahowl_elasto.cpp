82528c65e5a5cde628b4a5dba1a5f03143e7a136
mattcapa <matteo.capaldo@totalenergies.com>
2024-04-26 21:23:24 +0200

    Expose aero torque and accumulate elec torque

--- src/bindings/python/pyseahowl_elasto.cpp
+++ src/bindings/python/pyseahowl_elasto.cpp
@@ -203,6 +203,7 @@ void initialize_pyseahowl_elasto(py::module& m) {
         m_elasto, "RotorNacelleAssemblyElasto")
         .def(py::init<>())
         .def("get_rpm", &seahowl::elasto::RotorNacelleAssemblyElasto::get_rpm)
+        .def("accumulate_electrical_torque", &seahowl::elasto::RotorNacelleAssemblyElasto::accumulate_electrical_torque)
         .def("get_axial_thrust", &seahowl::elasto::RotorNacelleAssemblyElasto::get_axial_thrust)
         .def("get_axial_torque", &seahowl::elasto::RotorNacelleAssemblyElasto::get_axial_torque)
         .def("get_azimuth", &seahowl::elasto::RotorNacelleAssemblyElasto::get_azimuth)
