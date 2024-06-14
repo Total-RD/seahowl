cf0a9ebaed4d136d72666f4d68a0d5e2a1742897
Tristan de Lataillade <delataillade.tristan@gmail.com>
2024-06-14 15:47:19 +0200

    Add Python main file example

---
+++ examples/python/ex_main.py
@@ -0,0 +1,14 @@
+import seahowl
+
+# options
+main_filepath = "../../data/IEA15MW/main.json"  # change to actual filepath
+seahowl.set_log_level_global("info")  # log levels: critical, info, debug, warn, trace
+
+# make simulation object
+simulation = seahowl.core.Simulation()
+simulation.populate_from_file(main_filepath)
+simulation.initialize_from_file(main_filepath)
+
+# simulation loop
+while simulation.system_core.get_time() < simulation.duration:
+    simulation.step()
