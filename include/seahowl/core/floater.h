6b975be07234a349749ed7183d4dbec01b981f80
Tristan de Lataillade <delataillade.tristan@gmail.com>
2024-07-31 23:26:19 +0200

    Add Floater class to manage floaters

---
+++ include/seahowl/core/floater.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "seahowl/commons/utils.h"  // for DiscretizationPoint
+#include "seahowl/core/component.h"
+#include "seahowl/core/mooring.h"
+
+// forward declarations
+namespace seahowl {
+namespace core {
+class MooringSystem;
+}  // namespace core
+namespace elasto {
+class FloaterElasto;
+}  // namespace elasto
+namespace hydro {
+class FloaterHydro;
+}  // namespace hydro
+}  // namespace seahowl
+
+namespace seahowl {
+namespace core {
+
+class Foundation : public ComponentDynamic {};
+
+/**
+ * @brief Floater of wind turbine, with both elasto and hydro components.
+ *
+ */
+class Floater : public Foundation {
+  public:
+    /** @brief Elastodynamic model of the mooring. */
+    seahowl::elasto::FloaterElasto& elasto;
+    /** @brief Hydrodynamic model of the mooring. */
+    seahowl::hydro::FloaterHydro& hydro;
+    /** @brief Mooring system of the floater. */
+    std::unique_ptr<MooringSystem> mooring_system;
+
+    /**
+     * @brief Instantiates floater for communication between elasto and hydro components.
+     *
+     * @param[in] elasto Elastodynamic floater model.
+     * @param[in] aero Hydrodynamic floater model.
+     */
+    Floater(seahowl::elasto::FloaterElasto& elasto, seahowl::hydro::FloaterHydro& hydro);
+
+    /**
+     * @brief Prestep for floater, called before elastodynamic stepping.
+     *
+     * @param[in] time Time of the simulation.
+     * @param[in] dt Time step length.
+     */
+    void prestep(double time, double dt) override;
+
+    /**
+     * @brief Poststep for floater, called after elastodynamic stepping.
+     *
+     * @param[in] time Absolute time of the simulation.
+     * @param[in] dt Time step length.
+     */
+    void poststep(double time, double dt) override;
+
+    void apply_fluid_model(seahowl::env::FluidModel& fluid_model, double time) override;
+
+    /**
+     * @brief Builds the floater (hydro and elasto part).
+     */
+    void build() override;
+
+  private:
+    /**
+     * @brief Initialize floater, called before starting the simulation.
+     *
+     * Runs the preset and poststep once to make elasto and aero components match.
+     *
+     * @param[in] time Time of the simulation (usually 0 at init).
+     * @param[in] dt Time step length.
+     */
+    void initialize_this(double time, double dt) override;
+};
+
+}  // namespace core
+}  // namespace seahowl
---
+++ src/core/floater.cpp
@@ -0,0 +1,40 @@
+#include "seahowl/core/floater.h"
+
+#include "seahowl/core/mooring.h"
+#include "seahowl/elasto/floater_elasto.h"
+#include "seahowl/hydro/floater_hydro.h"
+
+#include <memory>
+#include <vector>
+#include <spdlog/spdlog.h>
+
+using namespace seahowl::core;
+using namespace seahowl::elasto;
+using namespace seahowl::hydro;
+
+Floater::Floater(FloaterElasto& elasto, FloaterHydro& hydro) : elasto(elasto), hydro(hydro) {
+    mooring_system = std::make_unique<MooringSystem>(*elasto.mooring_system, *hydro.mooring_system);
+}
+
+void Floater::initialize_this(double time, double dt) {
+    mooring_system->initialize(time, dt);
+
+    spdlog::info("Initialized floater of total mass {:.4}kg (with moorings).", elasto.get_mass());
+}
+
+void Floater::prestep(double time, double dt) {
+    mooring_system->prestep(time, dt);
+}
+
+void Floater::poststep(double time, double dt) {
+    mooring_system->poststep(time, dt);
+}
+
+void Floater::apply_fluid_model(seahowl::env::FluidModel& fluid_model, double time) {
+    mooring_system->apply_fluid_model(fluid_model, time);
+}
+
+void Floater::build() {
+    // build elasto
+    elasto.build();
+}
