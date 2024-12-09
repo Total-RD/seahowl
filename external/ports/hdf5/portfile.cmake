96733546f7230ccc7120ab438e47a25d6871c730
Tristan de Lataillade <delataillade.tristan@gmail.com>
2024-12-09 09:20:35 +0100

    Update vcpkg ports/dependencies + format json

--- external/ports/hdf5/portfile.cmake
+++ external/ports/hdf5/portfile.cmake
@@ -5,13 +5,13 @@ vcpkg_from_github(
     SHA512 87545f45ccd2c002569ffe2ffd07b615d53706bfbff07daaa71ff94704ebd2886458c4d7b512a8798a405d35d8a341968f25ad280e7a70e71a1c067e25c98743
     HEAD_REF master
 )
- 
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DHDF5_INSTALL_CMAKE_DIR=share/hdf5
         -DBUILD_SHARED_LIBS=OFF
-        -DHDF5_BUILD_CPP_LIB=ON 
+        -DHDF5_BUILD_CPP_LIB=ON
         -DHDF5_ENABLE_CXX=ON
         -DVCPKG_BUILD_TYPE=release
 )
@@ -20,4 +20,3 @@ vcpkg_cmake_config_fixup()
 vcpkg_copy_tools(TOOL_NAMES mirror_server mirror_server_stop h5copy h5diff h5dump h5ls h5stat gif2h5 h52gif h5clear h5debug
 h5format_convert h5jam h5unjam h5mkgrp h5repack h5repart h5watch h5import h5perf_serial
 AUTO_CLEAN)
-
--- external/ports/hydrochrono/portfile.cmake
+++ external/ports/hydrochrono/portfile.cmake
@@ -3,16 +3,14 @@ vcpkg_from_git(
     URL https://github.com/NREL/HydroChrono.git
     REF 43439c3b6dae2d195a7eb8e1dde440d406ca0e07
 )
- 
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DCMAKE_BUILD_TYPE=Release
         -DHYDROCHRONO_ENABLE_IRRLICHT=OFF
-        
+        -DHYDROCHRONO_ENABLE_DEMOS=OFF
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME HydroChrono
                          NO_PREFIX_CORRECTION )
-
-
