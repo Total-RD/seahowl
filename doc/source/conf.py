32c5dc6b8cd1904d118785cb9564a95ac05ebc23
Tristan de Lataillade <tristan.de-lataillade@totalenergies.com>
2025-03-06 20:41:41 +0100

    Bump SEAHOWL version to v0.9.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ copyright = "2022, TotalEnergies-SE"
 author = "Power R&D Team"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.1"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
