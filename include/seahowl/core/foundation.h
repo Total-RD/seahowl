9a3623dcef85864bc9bbe364cbdf8e9bede00c64
Jerome Lucas <jerome.lucas@cesgenslab.fr>
2025-05-28 19:58:03 +0200

    [FIX] minor issue

--- include/seahowl/core/foundation.h
+++ include/seahowl/core/foundation.h
@@ -7,12 +7,11 @@ namespace core {
 
 class Foundation : public virtual ComponentDynamic {
   public:
-    Foundation(const std::shared_ptr<seahowl::elasto::ComponentElasto>& elasto_,
-               const std::shared_ptr<seahowl::ComponentFluid>& fluid_)
+    Foundation(const std::shared_ptr<seahowl::elasto::ComponentElasto> elasto_,
+               const std::shared_ptr<seahowl::ComponentFluid> fluid_)
         : ComponentDynamic(elasto_, fluid_) {}
 
     virtual ~Foundation() = default;
-
 };
 
 }  // namespace core
