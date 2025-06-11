6d476951d282c7b72511f871d83d1edf012e751f
Jerome Lucas <jerome.lucas@cesgenslab.fr>
2025-06-11 15:26:13 +0200

    [ADD] add setting vscode in script setup

--- scripts/setup_vscode_env.bat
+++ scripts/setup_vscode_env.bat
@@ -13,3 +13,61 @@ for %%e in (
 ) do (
     code --install-extension %%e
 )
+
+:: Create .vscode directory if it doesn't exist
+if not exist "..\.vscode" (
+    mkdir "..\.vscode"
+)
+
+:: Create or overwrite the launch.json file
+> "..\.vscode\launch.json" (
+    echo {
+    echo     "version": "0.2.0",
+    echo     "configurations": [
+    echo     {
+    echo         "name": "(gdb) Launch",
+    echo         "type": "cppdbg",
+    echo         "request": "launch",
+    echo         "program": "${workspaceFolder}/build/debug/seahowl_driver",
+    echo         "args": ["../data/IEA15MW/onshore/main.json"],
+    echo         "stopAtEntry": false,
+    echo         "cwd": "${workspaceFolder}/build",
+    echo         "environment": [],
+    echo         "externalConsole": false,
+    echo         "MIMode": "gdb",
+    echo         "setupCommands": [
+    echo             {
+    echo                 "description": "Enable pretty-printing for gdb",
+    echo                 "text": "-enable-pretty-printing",
+    echo                 "ignoreFailures": true
+    echo             },
+    echo             {
+    echo                 "description": "Set Disassembly Flavor to Intel",
+    echo                 "text": "-gdb-set disassembly-flavor intel",
+    echo                 "ignoreFailures": true
+    echo             }
+    echo         ]
+    echo     }
+    echo     ]
+    echo }
+)
+
+:: Create or overwrite the settings.json file
+> "..\.vscode\settings.json" (
+    echo {
+    echo     "python.testing.unittestArgs": [
+    echo         "-v",
+    echo         "-s",
+    echo         "tests/non_regression",
+    echo         "-p",
+    echo         "*.py"
+    echo     ],
+    echo     "python.testing.pytestEnabled": false,
+    echo     "python.testing.unittestEnabled": true,
+    echo     "files.associations": {
+    echo         "deque": "cpp",
+    echo         "string": "cpp",
+    echo         "vector": "cpp"
+    echo     }
+    echo }
+)
--- scripts/setup_vscode_env.sh
+++ scripts/setup_vscode_env.sh
@@ -18,3 +18,57 @@ extensions=(
 for extension in "${extensions[@]}"; do
     code --install-extension "$extension"
 done
+
+# create .vscode directory if it doesn't exist
+mkdir -p ../.vscode
+# Create or overwrite the launch.json file
+cat <<EOL > ../.vscode/launch.json
+{
+    "version": "0.2.0",
+    "configurations": [
+    {
+        "name": "(gdb) Launch",
+        "type": "cppdbg",
+        "request": "launch",
+        "program": "${workspaceFolder}/build/debug/seahowl_driver",
+        "args": ["../data/IEA15MW/onshore/main.json"],
+        "stopAtEntry": false,
+        "cwd": "${workspaceFolder}/build",
+        "environment": [],
+        "externalConsole": false,
+        "MIMode": "gdb",
+        "setupCommands": [
+            {
+                "description": "Enable pretty-printing for gdb",
+                "text": "-enable-pretty-printing",
+                "ignoreFailures": true
+            },
+            {
+                "description": "Set Disassembly Flavor to Intel",
+                "text": "-gdb-set disassembly-flavor intel",
+                "ignoreFailures": true
+            }
+        ]
+    }
+    ]
+}
+EOL
+# Create or overwrite the settings.json file
+cat <<EOL > ../.vscode/settings.json
+{
+    "python.testing.unittestArgs": [
+        "-v",
+        "-s",
+        "tests/non_regression",
+        "-p",
+        "*.py"
+    ],
+    "python.testing.pytestEnabled": false,
+    "python.testing.unittestEnabled": true,
+    "files.associations": {
+        "deque": "cpp",
+        "string": "cpp",
+        "vector": "cpp"
+    },
+}
+EOL
