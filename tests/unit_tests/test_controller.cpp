7d8668de85332d50b1943bec2154ac8691a05c4b
Jerome Lucas <jerome.lucas@cesgenslab.fr>
2025-08-20 19:44:43 +0200

    [FIX] issue in TestController.individual_pitch_control

--- tests/unit_tests/test_controller.cpp
+++ tests/unit_tests/test_controller.cpp
@@ -173,10 +173,14 @@ TEST_F(TestController, individual_pitch_control) {
 
     // add wind model
     auto wind_model = std::make_shared<seahowl::env::ConstantWind>();
-    system_core.env_model->add_model(wind_model);
+
     wind_model->shear_coefficient = 0.12;
     wind_model->reference_height = turbine.elasto.rna->rotor->body_hub->get_position().z();
     wind_model->set_wind_velocity(seahowl::Vector3d(12.0, 0.0, 0.0));
+    // env_model
+    auto env_model = std::make_shared<seahowl::env::EnvModel>();
+    env_model->add_model(wind_model);
+    system_core.env_model = env_model;
 
     // initialize simulation
     simulation.initialize();
